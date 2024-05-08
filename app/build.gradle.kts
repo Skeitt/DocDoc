a11d8372fb2bbc1c986432f0a2665d2274dbb213
Gabriele Micaroni <116386904+EmmeGabri@users.noreply.github.com>
2024-05-08 21:09:25 +0200

    Configurazione Database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,8 @@
 plugins {
+    //id("com.android.application") == alias(libs.plugins.androidApplication) --> sono la stessa cosa
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
 }
@@ -36,6 +40,15 @@ android {
 }
 
 dependencies {
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+    implementation("com.google.firebase:firebase-auth")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
